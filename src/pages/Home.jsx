e9b5604b3226c678fbbca893bcf77385cb65427e
Nurul Islam <nurulbdpersonal@gmail.com>
2022-09-07 09:19:57 +0600

    Added Slider.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,13 +5,14 @@ import Announcement from "../components/Announcement";
 import Navbar from "../components/Navbar";
 // import Newsletter from "../components/Newsletter";
 // import Products from "../components/Products";
-// import Slider from "../components/Slider";
+import Slider from "../components/Slider";
 
 const Home = () => {
   return (
     <div>
     <Announcement />
     <Navbar />
+    <Slider />
     </div>
   );
 };
